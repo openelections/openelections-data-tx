27d82200ff700ce787d1c9fa22896adb76797958
Derek Willis <dwillis@gmail.com>
2017-10-21 14:53:25 -0400

    added Ruby parser for some CSV files

--- parser.rb
+++ parser.rb
@@ -1,22 +1,34 @@
 require 'remote_table'
+require 'roo-xls'
 require 'csv'
 
-t = RemoteTable.new("/Users/derekwillis/code/openelections-sources-tx/2016/Irion\ County.csv")
+county = 'Red River'
+t = RemoteTable.new("/Users/dwillis/code/openelections-sources-tx/2016/RedRiverCounty_DetailVoteTotals11082016.xlsx")
 rows = t.entries
 results = []
-offices = ['Straight Party', 'President and Vice President', 'United States Representative, District 11', 'Railroad Commissioner', 'State Senator, District 28', 'State Representative, District 72']
 headers = ['county', 'precinct', 'office', 'district', 'party', 'candidate', 'votes', 'early_votes', 'election_day']
 
 rows.each do |row|
-  # still need to add Over and Under Votes
-  if row['Contest_title'] ==  'Straight Party' and row['candidate_name'] == 'Republican Party'
-    results << ['Irion', row['Precinct_name'], 'Registered Voters', nil, nil, nil, row['Reg_voters'], nil, nil]
-    results << ['Irion', row['Precinct_name'], 'Ballots Cast', nil, nil, nil, row['total_ballots'], row['early_ballots'], row['election_ballots']]
+  if row['Contest_title'].include?(", District")
+    office, district = row['Contest_title'].split(", District ")
+  else
+    office = row['Contest_title']
+    district = nil
   end
-  results << ['Irion', row['Precinct_name'], row['Contest_title'], nil, row['Party_Code'], row['candidate_name'], row['total_votes'], row['early_votes'], row['election_ballots']]
+  if row['Contest_title'] == 'Straight Party' and row['candidate_name'].upcase == 'REPUBLICAN PARTY'
+    results << [county, row['Precinct_name'], office, district, nil, 'Over Votes', row['total_over_votes'], row['early_over_votes'], row['election_over_votes']]
+    results << [county, row['Precinct_name'], office, district, nil, 'Under Votes', row['total_under_votes'], row['early_under_votes'], row['election_under_votes']]
+    results << [county, row['Precinct_name'], 'Registered Voters', nil, nil, nil, row['Reg_voters'], nil, nil]
+    results << [county, row['Precinct_name'], 'Ballots Cast', nil, nil, nil, row['total_ballots'], row['early_ballots'], row['election_ballots']]
+  end
+  results << [county, row['Precinct_name'], office, district, row['Party_Code'], row['candidate_name'], row['total_votes'], row['early_votes'], row['election_ballots']]
+  if row['candidate_id'] == "1"
+    results << [county, row['Precinct_name'], office, district, nil, 'Over Votes', row['total_over_votes'], row['early_over_votes'], row['election_over_votes']]
+    results << [county, row['Precinct_name'], office, district, nil, 'Under Votes', row['total_under_votes'], row['early_under_votes'], row['election_under_votes']]
+  end
 end
 
-CSV.open("20161108__tx__general__irion__precinct.csv", "w") do |csv|
+CSV.open("20161108__tx__general__#{county.downcase.gsub(' ','_')}__precinct.csv", "w") do |csv|
   csv << headers
   results.map{|r| csv << r}
 end
