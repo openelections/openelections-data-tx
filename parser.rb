9751a845b0d36460c852b5ca1f28a0b67b57438a
Derek Willis <dwillis@gmail.com>
2018-06-09 20:01:19 -0400

    updated parsers

--- parser.rb
+++ parser.rb
@@ -2,11 +2,11 @@ require 'remote_table'
 require 'roo-xls'
 require 'csv'
 
-county = 'Karnes'
-t = RemoteTable.new("/Users/dwillis/code/openelections-sources-tx/2014/KARNES_COUNTY-2014_Republican_Party_Primary_Election_342014-DetailVoteTotals03-14-14_Republican.xlsx")
+county = 'Travis'
+t = RemoteTable.new("/Users/derekwillis/code/openelections-sources-tx/2018/Travis County TLC Report.xlsx")
 rows = t.entries
 results = []
-headers = ['county', 'precinct', 'office', 'district', 'party', 'candidate', 'votes', 'early_votes', 'election_day']
+headers = ['county', 'precinct', 'office', 'district', 'party', 'candidate', 'votes', 'early_votes', 'election_day', 'absentee']
 
 rows.each do |row|
   if row['Contest_title'].include?(", District")
@@ -15,20 +15,20 @@ rows.each do |row|
     office = row['Contest_title']
     district = nil
   end
-  if row['Contest_title'].upcase == 'UNITED STATES SENATOR' and row['candidate_id'].to_i == 1
-    results << [county, row['Precinct_name'], office, district, row['Party_Code'], 'Over Votes', row['total_over_votes'], row['early_over_votes'], row['election_over_votes']]
-    results << [county, row['Precinct_name'], office, district, row['Party_Code'], 'Under Votes', row['total_under_votes'], row['early_under_votes'], row['election_under_votes']]
-    results << [county, row['Precinct_name'], 'Registered Voters', nil, row['Party_Code'], nil, row['Reg_voters'], nil, nil]
-    results << [county, row['Precinct_name'], 'Ballots Cast', nil, row['Party_Code'], nil, row['total_ballots'], row['early_ballots'], row['election_votes']]
+  if row['Contest_title'].upcase == 'UNITED STATES SENATOR - DEM' and row['candidate_name'] == 'Edward Kimbrough'
+    results << [county, row['Precinct_name'], office, district, row['Party_Code'], 'Over Votes', row['total_over_votes'], row['early_over_votes'], row['election_over_votes'], row['absentee_over_votes']]
+    results << [county, row['Precinct_name'], office, district, row['Party_Code'], 'Under Votes', row['total_under_votes'], row['early_under_votes'], row['election_under_votes'], row['absentee_under_votes']]
+#    results << [county, row['Precinct_name'], 'Registered Voters', nil, row['Party_Code'], nil, row['total_ballots'], nil, nil]
+    results << [county, row['Precinct_name'], 'Ballots Cast', nil, row['Party_Code'], nil, row['total_ballots'], row['early_ballots'], row['election_ballots'], row['absentee_ballots']]
   end
-  results << [county, row['Precinct_name'], office, district, row['Party_Code'], row['candidate_name'], row['total_votes'], row['early_votes'], row['election_votes']]
+  results << [county, row['Precinct_name'], office, district, row['Party_Code'], row['candidate_name'], row['total_votes'], row['early_votes'], row['election_votes'], row['']]
   if row['candidate_id'] == "1"
     results << [county, row['Precinct_name'], office, district, row['Party_Code'], 'Over Votes', row['total_over_votes'], row['early_over_votes'], row['election_over_votes']]
     results << [county, row['Precinct_name'], office, district, row['Party_Code'], 'Under Votes', row['total_under_votes'], row['early_under_votes'], row['election_under_votes']]
   end
 end
 
-CSV.open("20140304__tx__primary__#{county.downcase.gsub(' ','_')}__precinct.csv", "w") do |csv|
+CSV.open("20180306__tx__primary__#{county.downcase.gsub(' ','_')}__precinct.csv", "w") do |csv|
   csv << headers
   results.map{|r| csv << r}
 end
--- parser2.rb
+++ parser2.rb
@@ -2,8 +2,8 @@ require 'remote_table'
 require 'roo-xls'
 require 'csv'
 
-county = 'Wilbarger'
-t = RemoteTable.new("/Users/dwillis/code/openelections-sources-tx/2014/WILBARGER_COUNTY-2014_General_Election_1142014-votes 11-4-2014 final.csv")
+county = 'Tarrant'
+t = RemoteTable.new("/Users/derekwillis/code/openelections-sources-tx/2018/TARRANT_COUNTY-2018_Democratic_Party_Primary_Election_362018-SOSDEMPrecinctReport.xlsx")
 rows = t.entries
 results = []
 headers = ['county', 'precinct', 'office', 'district', 'party', 'candidate', 'votes', 'early_votes', 'election_day']
@@ -15,14 +15,14 @@ rows.each do |row|
     office = row['race_name']
     district = nil
   end
-  if row['race_name'] == 'Straight Party' and row['candidate_name'].upcase == 'REPUBLICAN PARTY'
+  if row['race_name'] == 'United States Senator' and row['candidate_name'] == 'Beto O\'Rourke'
     results << [county, row['Precinct_name'], 'Registered Voters', nil, nil, nil, row['Reg_voters'], nil, nil]
   end
   total_votes = row['early_votes'].to_i + row['election_votes'].to_i
   results << [county, row['Precinct_name'], office, district, row['Party_Code'], row['candidate_name'], total_votes, row['early_votes'], row['election_votes']]
 end
 
-CSV.open("20141104__tx__general__#{county.downcase.gsub(' ','_')}__precinct.csv", "w") do |csv|
+CSV.open("20180306__tx__primary__#{county.downcase.gsub(' ','_')}__precinct.csv", "w") do |csv|
   csv << headers
   results.map{|r| csv << r}
 end
