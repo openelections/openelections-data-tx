7c6048509e83c6149e825b6bf6f4eca564810f1e
B Davis <rdavis00027@gmail.com>
2022-08-17 01:33:04 -0700

    add code to parse some CSV and XLSX files

--- r-parsers/tx22primary_all.R
+++ r-parsers/tx22primary_all.R
@@ -73,7 +73,7 @@ for (f in list){
         #######################################################################
         # Check for known fixed-width formats 
         #######################################################################
-        if (party != "" & ext %in% c(".txt",".ASC")){
+        if (party != "" & toupper(ext) %in% c(".TXT",".ASC")){
             file_txt <- paste0(dir,f)
             rr <- readLines(file_txt)
             nc <- nchar(rr[1])
@@ -108,6 +108,106 @@ for (f in list){
                 gotxx <- FALSE
             }
         }
+        #######################################################################
+        # Check for all known csv and xlsx formats
+        #######################################################################
+        else if (party != "" & toupper(ext) %in% c(".CSV",".XLSX")){
+            dd <- def[toupper(def$counties) == toupper(county),]
+            if (is.na(dd$desc[1]) | dd$desc[1] == "NA") next
+            gotxx <- FALSE
+            if (NROW(dd) == 1){
+                if (!is.na(dd$desc2[1]) & dd$desc2[1] == "long"){ # load files only for long format, for now
+                    filename <- paste0(dir,f)
+                    nskip <- 0
+                    if (!is.na(dd$skip[1])){
+                        nskip <- as.numeric(dd$skip[1])
+                    }
+                    if (toupper(ext) == ".CSV"){
+                        vv <- read_csv(filename, col_types = "c", skip = nskip)
+                    }
+                    else{
+                        vv <- read_excel(filename, sheet = dd$sheet[1], skip = nskip)
+                    }
+                    zvv <<- vv #DEBUG
+                }
+                if (!is.na(dd$desc2[1]) & dd$desc2[1] == "long"){
+                    xx <- vv[c(dd$precinct[1],dd$office[1],dd$candidate[1])]
+                    names(xx) <- c("precinct","office","candidate")
+                    xx$county <- str_to_title(county) # match standard
+                    xx$district <- ""
+                    if (!is.na(dd$party[1]) & substr(dd$party[1],1,1) != "|"){
+                        xx$party <- vv[[dd$party[1]]]
+                    }
+                    else{
+                        xx$party <- party
+                    }
+                    nmsxx <- c("county","precinct","office","district","party","candidate")
+                    xx <- xx[nmsxx]
+                    #nmsxx <- c("county","precinct","office","district","party","candidate","votes",
+                    #           "absentee","early_voting","election_day","provisional")
+                    xvotes <- FALSE
+                    xx$votes <- 0
+                    if (!is.na(dd$absentee[1])){
+                        xx$absentee <- as.numeric(vv[[dd$absentee[1]]])
+                        xx$absentee[is.na(xx$absentee)] <- 0
+                        xx$votes <- xx$votes + xx$absentee
+                        got_absentee <- TRUE
+                        xvotes <- TRUE
+                    }
+                    else{
+                        xx$absentee <- 0
+                        got_absentee <- FALSE
+                    }
+                    if (!is.na(dd$early_voting[1])){
+                        xx$early_voting <- as.numeric(vv[[dd$early_voting[1]]])
+                        xx$early_voting[is.na(xx$early_voting)] <- 0
+                        xx$votes <- xx$votes + xx$early_voting
+                        got_early_voting <- TRUE
+                        xvotes <- TRUE
+                    }
+                    else{
+                        xx$early_voting <- 0
+                        got_early_voting <- FALSE
+                    }
+                    if (!is.na(dd$election_day[1])){
+                        xx$election_day <- as.numeric(vv[[dd$election_day[1]]])
+                        xx$election_day[is.na(xx$election_day)] <- 0
+                        xx$votes <- xx$votes + xx$election_day
+                        got_election_day <- TRUE
+                        xvotes <- TRUE
+                    }
+                    else{
+                        xx$election_day <- 0
+                        got_election_day <- FALSE
+                    }
+                    if (!is.na(dd$provisional_counted[1])){
+                        xx$provisional <- as.numeric(vv[[dd$provisional_counted[1]]])
+                        xx$provisional[is.na(xx$provisional)] <- 0
+                        xx$votes <- xx$votes + xx$provisional
+                        got_provisional <- TRUE
+                        xvotes <- TRUE
+                    }
+                    else{
+                        xx$provisional <- 0
+                        got_provisional <- FALSE
+                    }
+                    if (!is.na(dd$votes[1])){
+                        xx$votes <- vv[[dd$votes[1]]]
+                        got_votes <- TRUE
+                    }
+                    gotxx <- TRUE
+                }
+                else{
+                    gotxx <- FALSE
+                }
+                xx <- xx[!is.na(xx$office),] # remove blank lines for Austin County
+                zdd <<- dd #DEBUG-RM
+                zxx <<- xx #DEBUG-RM
+            }
+            else{
+                gotxx <- FALSE
+            }
+        }
         gxx1 <<- xx #DEBUG-RM
         if (gotxx){
             file_csv <- paste0(dir,"out/",f)
@@ -119,6 +219,8 @@ for (f in list){
             #xx$precinct <- gsub("^PCT ","",xx$precinct, ignore.case = TRUE) # Wharton County
             xx$party[xx$party == "(D)"] <- "DEM" # El Paso County
             xx$party[xx$party == "(R)"] <- "REP" # El Paso County
+            xx$party[grepl("^Democratic",xx$party, ignore.case = TRUE)] <- "DEM" #Brazos County
+            xx$party[grepl("^Republican",xx$party, ignore.case = TRUE)] <- "REP" #Brazos County
             xx$office <- str_to_title(xx$office)
             
             xx$office[xx$office == "Registered Voters - Total"] <- "Registered Voters"
@@ -137,42 +239,43 @@ for (f in list){
             xx$party[grepl("^Dem ",xx$office, ignore.case = TRUE)] <- "DEM"
             xx$office <- gsub("^\\(R\\) ","",xx$office, ignore.case = TRUE) # El Paso County
             xx$office <- gsub("^\\(D\\) ","",xx$office, ignore.case = TRUE) # El Paso County
-            xx$office <- gsub("^Rep ","",xx$office, ignore.case = TRUE)
-            xx$office <- gsub("^Dem ","",xx$office, ignore.case = TRUE)
+            xx$office <- gsub("^Rep\\.? ","",xx$office, ignore.case = TRUE)
+            xx$office <- gsub("^Dem\\.? ","",xx$office, ignore.case = TRUE)
             xx$office <- gsub("^Rep ","",xx$office, ignore.case = TRUE) # delete double Rep
             xx$office <- gsub("^Dem ","",xx$office, ignore.case = TRUE) # delete double Dem
             xx$office <- gsub(" Of "," of ",xx$office)
             xx$office <- gsub(" At "," at ",xx$office)
+            xx$office <- gsub("-At-","-at-",xx$office) # Austin County
             xx$office <- gsub(" The "," the ",xx$office) # Clay County
             xx$office <- gsub(" And "," and ",xx$office) # Goliad County
             xx$office <- gsub("^Jop ","JOP ",xx$office) # Donley County
             xx$office <- gsub("State Boe","State BoE",xx$office)
             xx$office <- gsub("State Senator","State Senate",xx$office) # Clay County
-            xx$office <- gsub("^Us Representative","U.S. House",xx$office, ignore.case = TRUE)
-            xx$office <- gsub("^Us Rep,","U.S. House,",xx$office, ignore.case = TRUE) # Goliad County
+            xx$office <- gsub("^U\\.? ?s\\.? Representative","U.S. House",xx$office, ignore.case = TRUE) # Brazos County
+            xx$office <- gsub("^U\\.? ?s\\.? Rep,","U.S. House,",xx$office, ignore.case = TRUE) # Goliad County
             xx$office <- gsub("^United States Representative","U.S. House",xx$office, ignore.case = TRUE) # Guadalupe County
             xx$office <- gsub("^County  Commissioner","County Commissioner",xx$office, ignore.case = TRUE) # Guadalupe County
             
             for (i in 1:NROW(xx)){
-                mm <- str_match(xx$office[i], "U.S. House, Dist(?:rict)? (\\d+)")
+                mm <- str_match(xx$office[i], "U.S. House\\,? Dist\\.?(?:rict)? (\\d+)")
                 if(!is.na(mm[1,1])){
                     xx$district[i] <- mm[1,2]
                     xx$office[i] <- "U.S. House"
                     next
                 }
-                mm <- str_match(xx$office[i],"Us Rep(?: \\d+ Us),* Dist(?:rict)? (\\d+)")
+                mm <- str_match(xx$office[i],"Us Rep(?: \\d+ Us)\\,? Dist\\.?(?:rict)? (\\d+)")
                 if(!is.na(mm[1,1])){
                     xx$district[i] <- mm[1,2]
                     xx$office[i] <- "U.S. House"
                     next
                 }
-                mm <- str_match(xx$office[i], "State Senate, Dist(?:rict)? (\\d+)")
+                mm <- str_match(xx$office[i], "State Senate\\,? Dist\\.?(?:rict)? (\\d+)")
                 if(!is.na(mm[1,1])){
                     xx$district[i] <- mm[1,2]
                     xx$office[i] <- "State Senate"
                     next
                 }
-                mm <- str_match(xx$office[i], "State Representative, Dist(?:rict)? (\\d+)")
+                mm <- str_match(xx$office[i], "State Representative\\,? Dist\\.?(?:rict)? (\\d+)")
                 if(!is.na(mm[1,1])){
                     xx$district[i] <- mm[1,2]
                     xx$office[i] <- "State House"
@@ -226,9 +329,54 @@ for (f in list){
             
             xx$party[is.na(xx$party)] <- ""
             if (xvotes){
-                sumprov <- sum(xx$provisional)
+                # delete provisional if their sum = 0
+                iprov <- which(names(xx) == "provisional")
+                sumprov <- sum(xx$provisional, na.rm = TRUE)
                 if (sumprov == 0){
-                    xx <- xx[-NCOL(xx)]
+                    xx <- xx[-iprov]
+                }
+                # delete absentee if their sum = 0
+                iabsent <- which(names(xx) == "absentee")
+                sumabsent <- sum(xx$absentee, na.rm = TRUE)
+                if (sumabsent == 0){
+                    xx <- xx[-iabsent]
+                }
+            }
+            # fix missing precincts if surrounded by same precinct - Austin County
+            ipna <- which(is.na(xx$precinct))
+            lpna <- length(ipna)
+            if (lpna > 0){
+                for (i in 1:lpna){
+                    if (ipna[i] > 1 & ipna[i] < NROW(xx)){
+                        if (xx$precinct[ipna[i]-1] == xx$precinct[ipna[i]+1]){
+                            xx$precinct[ipna[i]] <- xx$precinct[ipna[i]-1]
+                        }
+                    }
+                }
+            }
+            zxx <<- xx #DEBUG
+            # tests to replicate prior files
+            test_replicates <- FALSE
+            if (test_replicates){
+                if (toupper(county) == "AUSTIN"){
+                    xx$office <- gsub("State House","State Representative",xx$office)
+                    nmsxx <- c("county","precinct","office","district","candidate","party",
+                               "early_voting","election_day","votes")
+                    xx <- xx[nmsxx]
+                }
+                else if (toupper(county) == "BAILEY"){
+                    nmsxx <- c("county","precinct","office","district","candidate","party",
+                               "early_voting","election_day","provisional","votes")
+                    xx <- xx[nmsxx]
+                    xx <- xx[-which(names(xx) == "provisional")] #DEBUG-DUPLICATE
+                    xx$office <- toupper(xx$office)
+                    xx$office <- gsub("U.S. HOUSE","U.S. House",xx$office)
+                    xx$office <- gsub("COMM GENERAL LAND OFFICE","COMM GENERAL LAND OFFICE ",xx$office)
+                    xx$candidate <- gsub("KEN PAXTON","KEN PAXTON ",xx$candidate)
+                    xx$candidate <- gsub("JAMES WHITE","JAMES WHITE ",xx$candidate)
+                    xx$candidate <- gsub("JOY DIAZ","JOY DIAZ ",xx$candidate)
+                    xx$office <- gsub("STATE SENATE","State Senate",xx$office)
+                    xx$office <- gsub("STATE HOUSE","State Representative",xx$office)
                 }
             }
             write_csv(xx, file_csv)
